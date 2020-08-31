1e0889e69b7a5d807517fc39b979c945ddf65610
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-31 19:09:07 -0300

    Connecting user route with userController

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// Create server
+const app = express();
+
+//Enable express.json
+app.use(express.json({ extended: true }));
+
+// App port
+const port = process.env.PORT || 4000;
+
+// Routes
+app.use("/api/users", require("./routes/users"));
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is working on port ${port}`);
+});
