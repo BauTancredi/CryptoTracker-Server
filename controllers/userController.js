2a23f88604ad1b5f57ef569c534223895e0b7080
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-08-31 19:20:57 -0300

    Creating and saving User model

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,18 @@
+const User = require("../models/User");
+
 exports.createUser = async (req, res) => {
-  console.log(req.body);
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ msg: "The user already exists" });
+
+    // Create new user
+    let user = new User(req.body);
+
+    await user.save();
+  } catch (error) {
+    console.log(error);
+  }
 };
