60e87e389507bf3a359dc9e28907c9182a8ba015
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-02 17:33:15 -0300

    authController and authenticateUser method

---
+++ controllers/authController.js
@@ -0,0 +1,51 @@
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+exports.authenticateUser = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty())
+    return res.status(400).json({ errors: errors.array() });
+
+  const { email, password } = req.body;
+
+  try {
+    // Verify user exists
+    let user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ msg: "The user does not exists" });
+
+    // Verify password
+    const validPassword = await bcryptjs.compare(password, user.password);
+
+    if (!validPassword)
+      return res.status(400).json({ msg: "Incorrect password" });
+
+    // Create JWT
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    // Sign JWT
+    jwt.sign(
+      payload,
+      process.env.SECRET,
+      {
+        expiresIn: 3600,
+      },
+      (error, token) => {
+        if (error) throw error;
+
+        // Confirmation message
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,21 @@
 // Routes for authenticate users
 const express = require("express");
+const { check } = require("express-validator");
 
 const authController = require("../controllers/authController");
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 
 const router = express.Router();
 
 // Authenticate user
 // api/auth
-router.post("/", authController.authenticateUser);
+router.post(
+  "/",
+  check("email", "The email format is not valid").isEmail(),
+  authController.authenticateUser
+);
 
 // Obtains authenticated user
-router.get("/", auth, authController.authenticatedUser);
+// router.get("/", auth, authController.authenticatedUser);
 
 module.exports = router;
