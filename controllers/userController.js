30398918257a04795bb81b2e2b619b454b4ea144
Bautista Tancredi <bautistatancredi@hotmail.com>
2020-09-02 17:05:22 -0300

    Connecting DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,12 +4,12 @@ exports.createUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    let user = await User.findOne({ email });
+    // let user = await User.findOne({ email });
 
-    if (user) return res.status(400).json({ msg: "The user already exists" });
+    // if (user) return res.status(400).json({ msg: "The user already exists" });
 
-    // Create new user
-    let user = new User(req.body);
+    // // Create new user
+    // let user = new User(req.body);
 
     await user.save();
   } catch (error) {
--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 // Create server
 const app = express();
+
+// Connect to DB
+connectDB();
 
 //Enable express.json
 app.use(express.json({ extended: true }));
